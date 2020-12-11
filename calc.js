a18098ada5110ea5b06127862cc295b63e6a63c5
Willian Lopes <williangldzn@gmail.com>
2020-12-10 22:44:18 -0300

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,20 @@
+function soma(x, y) {
+  return x + y;
+};
+
+function sub(x, y) {
+  return x - y;
+};
+
+function mul(x, y) {
+  return x * y;
+};
+
+function div(x, y) {
+  return x / y
+};
+
+exports.soma = soma;
+exports.sub = sub;
+exports.mul = mul;
+exports.div = div;
